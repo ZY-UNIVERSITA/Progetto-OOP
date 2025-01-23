31b081bff346572776be8c52a51d88d8c3ff7f84
ZY <yuhang.zhu@studio.unibo.it>
2025-01-23 12:27:06 +0100

    Add bouncy castle library to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,6 +47,9 @@ dependencies {
     // JUnit API and testing engine
     testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+
+    // Bouncy castle
+    implementation("org.bouncycastle:bcprov-jdk18on:1.80")
 }
 
 tasks.withType<Test> {
