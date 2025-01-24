8602a1719a750bc08dfedcedd6e827271ff79996
ZY <yuhang.zhu@studio.unibo.it>
2025-01-24 08:50:36 +0100

    Add to string and equals methods to user account

--- src/main/java/com/zysn/passwordmanager/model/account/entity/UserAccount.java
+++ src/main/java/com/zysn/passwordmanager/model/account/entity/UserAccount.java
@@ -40,4 +40,51 @@ public class UserAccount {
     public void setMasterKey(SecretKeySpec masterKey) {
         this.masterKey = masterKey;
     }
+
+    /* EQUALS */
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        result = prime * result + ((algorithmConfig == null) ? 0 : algorithmConfig.hashCode());
+        result = prime * result + ((masterKey == null) ? 0 : masterKey.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UserAccount other = (UserAccount) obj;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        if (algorithmConfig == null) {
+            if (other.algorithmConfig != null)
+                return false;
+        } else if (!algorithmConfig.equals(other.algorithmConfig))
+            return false;
+        if (masterKey == null) {
+            if (other.masterKey != null)
+                return false;
+        } else if (!masterKey.equals(other.masterKey))
+            return false;
+        return true;
+    }
+
+    /* TO STRING */
+
+    @Override
+    public String toString() {
+        return "UserAccount [username=" + username + ", algorithmConfig=" + algorithmConfig + ", masterKey=" + masterKey
+                + "]";
+    }    
 }
