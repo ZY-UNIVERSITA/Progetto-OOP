d17282f8fdbb6cdeb3695bf4b9f115decf719f65
ZY-UNIVERSITA <yuhang.zhu@studio.unibo.it>
2025-02-04 15:30:12 +0100

    Add Login controller skeleton

---
+++ src/main/java/com/zysn/passwordmanager/controller/scene/impl/Login.java
@@ -0,0 +1,45 @@
+/**
+ * Sample Skeleton for 'Login.fxml' Controller Class
+ */
+
+package com.zysn.passwordmanager.controller.scene.impl;
+
+public class LoginController extends SceneControllerBase {
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="mainVBox"
+    private VBox mainVBox; // Value injected by FXMLLoader
+
+    @FXML // fx:id="passwordField"
+    private PasswordField passwordField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="usernameField"
+    private TextField usernameField; // Value injected by FXMLLoader
+
+    @FXML
+    void handleLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleRegister(ActionEvent event) {
+        this.getViewNavigator().navigateTo("/layouts/Register.fxml", "Registration");
+    }
+
+    @FXML
+    void handleRestore(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert mainVBox != null : "fx:id=\"mainVBox\" was not injected: check your FXML file 'Login.fxml'.";
+        assert passwordField != null : "fx:id=\"passwordField\" was not injected: check your FXML file 'Login.fxml'.";
+        assert usernameField != null : "fx:id=\"usernameField\" was not injected: check your FXML file 'Login.fxml'.";
+    }
+
+}
