e06544181145fa78fa6c15f22ab9b2f8cf6f460d
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2025-02-09 14:46:58 +0100

    Add: overloaded navigateTo method to pass Service

--- src/main/java/com/zysn/passwordmanager/controller/navigation/ViewNavigator.java
+++ src/main/java/com/zysn/passwordmanager/controller/navigation/ViewNavigator.java
@@ -10,6 +10,7 @@ import java.nio.file.Path;
 
 import com.zysn.passwordmanager.controller.scene.api.SceneController;
 import com.zysn.passwordmanager.model.account.manager.AccountManager;
+import com.zysn.passwordmanager.model.service.Service;
 
 /**
  * The ViewNavigator class is responsible for managing the navigation between different scenes in the application.
@@ -66,6 +67,43 @@ public class ViewNavigator {
                 
                 controller.setViewNavigator(this);
                 controller.setAccountManager(accountManager);
+            }
+
+            stage.setScene(new Scene(root));
+            stage.setTitle(sceneTitle);
+            stage.show();
+        } catch (IOException e) {
+            System.err.println("An error occurred while trying to change scene.");
+        }
+    }
+
+    /**
+     * Navigates to a new scene specified by the path to the FXML file, the scene title, and a Service object to the target scene's controller.
+     *
+     * @param pathToFile the path to the FXML file of the new scene (as a string)
+     * @param sceneTitle the title of the new scene
+     * @param service the Service object to be passed to the target controller
+     * @throws IllegalArgumentException if the pathToFile or sceneTitle is null
+     */
+    public void navigateTo(String pathToFile, String sceneTitle, Service service) {
+        if (pathToFile == null || sceneTitle == null) {
+            throw new IllegalArgumentException("The arguments cannot be null.");
+        }
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(pathToFile));
+            Parent root = loader.load();
+
+            if (loader.getController() instanceof SceneController) {
+                SceneController controller = loader.getController();
+                controller.setViewNavigator(this);
+                controller.setAccountManager(accountManager);
+                
+                // Pass the Service to ServiceManagerView
+                if (controller instanceof ServiceManagerController) {
+                    ServiceManagerController serviceController = (ServiceManagerController) controller;
+                    serviceController.setService(service);
+                }
             }
 
             stage.setScene(new Scene(root));
