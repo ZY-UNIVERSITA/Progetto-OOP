c27c690cf2ca7f0da8405fbf51f4794fe5ad6392
ZY <yuhang.zhu@studio.unibo.it>
2025-02-21 12:39:34 +0100

    Refactor key store manager to act as Facade
    Refactor key store manager to act as Facade:
    
    1. Updated DefaultKeyStoreManager to act as a Facade, encapsulating the complex subsystem of key store operations.
    2. Introduced interfaces for KeyStoreConfigService, KeyStoreCreator, KeyStoreStorageService, and KeyStoreEntryService to folllow SOLID.
    3. Simplified DefaultKeyStoreManager's methods to delegate tasks to the appropriate service classes, improving readability and maintainability.
    4. Adding javadoc and tests.

--- src/main/java/com/zysn/passwordmanager/model/authentication/keystore/impl/DefaultKeyStoreManager.java
+++ src/main/java/com/zysn/passwordmanager/model/authentication/keystore/impl/DefaultKeyStoreManager.java
@@ -1,63 +1,57 @@
 package com.zysn.passwordmanager.model.authentication.keystore.impl;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
-import javax.crypto.spec.SecretKeySpec;
-
+import com.zysn.passwordmanager.model.account.manager.api.SessionManager;
+import com.zysn.passwordmanager.model.authentication.keystore.api.KeyStoreCreator;
+import com.zysn.passwordmanager.model.authentication.keystore.api.KeyStoreEntryService;
 import com.zysn.passwordmanager.model.authentication.keystore.api.KeyStoreManager;
-import com.zysn.passwordmanager.model.authentication.keystore.api.KeyStoreService;
+import com.zysn.passwordmanager.model.authentication.keystore.api.KeyStoreConfigService;
+import com.zysn.passwordmanager.model.authentication.keystore.api.KeyStoreStorageService;
+import com.zysn.passwordmanager.model.enums.ExtensionsConstant;
 import com.zysn.passwordmanager.model.enums.PathsConstant;
-import com.zysn.passwordmanager.model.utils.crypto.impl.CryptoUtils;
+import com.zysn.passwordmanager.model.security.algorithm.config.impl.AlgorithmConfig;
+import com.zysn.passwordmanager.model.utils.crypto.CryptoUtils;
+import com.zysn.passwordmanager.model.utils.data.DataUtils;
 import com.zysn.passwordmanager.model.utils.encoding.EncodingUtils;
 import com.zysn.passwordmanager.model.utils.file.api.FileManager;
-
+import com.zysn.passwordmanager.model.utils.file.impl.DefaultFileManager;
 /**
- * The DefaultKeyStoreManager class is responsible for managing the key store operations.
+ * The DefaultKeyStoreManager class is responsible for managing the key store
+ * operations.
  * It implements the KeyStoreManager interface.
  */
 public class DefaultKeyStoreManager implements KeyStoreManager {
 
     private FileManager fileManager;
     private KeyStore keyStore;
-    private KeyStoreService keyStoreService;
-    private KeyStoreConfig keyStoreConfiguration;
+
+    private SessionManager sessionManager;
+
+    private KeyStoreConfigService keyStoreService;
+    private KeyStoreCreator keyStoreCreator;
+    private KeyStoreStorageService keyStoreStorage;
+    private KeyStoreEntryService keyStoreEntryManager;
+
+    private static final String TOTP_ENCRYPTION_KEY_ALIAS = "TOTP Encryption Key";
+    private static final String TOTP_KEY_ALIAS = "TOTP Key";
 
     /**
-     * Constructs a new DefaultKeyStoreManager with the specified FileManager and KeyStoreConfig.
+     * Initializes a new DefaultKeyStoreManager instance with the given session manager.
      *
-     * @param fileManager the FileManager to manage file operations
-     * @param keyStoreConfiguration the KeyStoreConfig for key store configuration
+     * @param sessionManager the session manager to be used for key store operations
      */
-    public DefaultKeyStoreManager(FileManager fileManager, KeyStoreConfig keyStoreConfiguration) {
-        this.fileManager = fileManager;
-        this.keyStoreConfiguration = keyStoreConfiguration;
-        this.keyStoreService = new DefaultKeyStoreService(keyStoreConfiguration);
-    }
+    public DefaultKeyStoreManager(SessionManager sessionManager) {
+        this.fileManager = new DefaultFileManager(PathsConstant.KEY_STORE, ExtensionsConstant.BCFKS);
 
-    /**
-     * Constructs a new DefaultKeyStoreManager with the specified FileManager and a default KeyStoreConfig.
-     *
-     * @param fileManager the FileManager to manage file operations
-     */
-    public DefaultKeyStoreManager(FileManager fileManager) {
-        this(fileManager, new KeyStoreConfig());
-    }
+        this.sessionManager = sessionManager;
 
-    /**
-     * Constructs a new DefaultKeyStoreManager with a default FileManager and KeyStoreConfig.
-     */
-    public DefaultKeyStoreManager() {
-        this(null);
+        this.keyStoreService = new DefaultKeyStoreConfigService();
+        this.keyStoreStorage = new DefaultKeyStoreStorageService();
+        this.keyStoreCreator = new DefaultKeyStoreCreator();
+        this.keyStoreEntryManager = new DefaultKeyStoreEntryService();
     }
 
     /**
@@ -65,183 +59,160 @@ public class DefaultKeyStoreManager implements KeyStoreManager {
      */
     @Override
     public void createNewKeyStore() {
-        this.keyStoreService.generateKeyStoreEncryptionKey();
+        char[] keyStorePassword = this.sessionManager.getKeyStoreConfig().getKeyStoreEncryptionKeyChar();
 
-        char[] keyStorePassword = EncodingUtils
-                .byteToCharConverter(this.keyStoreConfiguration.getKeyStoreEncryptionKey());
-
-        try {
-            this.keyStore = KeyStore.getInstance("BCFKS", "BC");
-            this.keyStore.load(null, keyStorePassword);
-        } catch (IOException | NoSuchAlgorithmException | CertificateException | KeyStoreException
-                | NoSuchProviderException e) {
-            System.err.println("An error occurred while trying to create the key store.");
-        } finally {
-            CryptoUtils.cleanMemory(keyStorePassword);
-        }
+        this.keyStore = this.keyStoreCreator.createKeyStore(keyStorePassword);
     }
 
     /**
-     * Creates key store entries for TOTP encryption key, TOTP key, and salt.
+     * Generates the key store configuration key and salt.
      */
     @Override
+    public void createKeyStoreConfig() {
+        this.keyStoreService.generateKeyStoreConfigKey(this.sessionManager.getKeyStoreConfig());
+        this.keyStoreService.generateKeyStoreConfigSalt(this.sessionManager.getKeyStoreConfig());
+    }
+
+    /**
+     * Generates a key store entry using the user authentication key.
+     */
     public void createKeyStoreEntry() {
-        this.keyStoreService.generateTotpEncryptionKey();
-        this.keyStoreService.generateTotpKey();
-        this.keyStoreService.generateSalt();
+        this.keyStoreService.generateKeyStoreEntry(this.sessionManager.getUserAuthKey());
     }
 
     /**
      * Populates the new key store with the provided password and keys.
-     *
-     * @param password the password to protect the key store
      */
     @Override
-    public void populateNewKeyStore(byte[] password) {
-        String totpEncryptionKeyAlias = "TOTP Encryption Key";
-        String totpKeyAlias = "TOTP Key";
+    public void populateNewKeyStore() {
 
-        this.insertKey(totpEncryptionKeyAlias, this.keyStoreConfiguration.getTotpEncryptionKey(), true, password,
-                this.keyStoreConfiguration.getSaltWithPasswordDerived());
+        char[] totpEncryptionKeyPassword = DataUtils.concatArray(
+                this.sessionManager.getUserAuthKey().getPasswordDerivedKeyChar(),
+                this.sessionManager.getKeyStoreConfig().getSaltWithPasswordDerivedChar());
 
-        this.insertKey(totpKeyAlias, this.keyStoreConfiguration.getTotpKey(), false,
-                this.keyStoreConfiguration.getTotpEncryptionKey(),
-                this.keyStoreConfiguration.getSaltWithTotpEncryptionKey());
-    }
-
-    /**
-     * Inserts a key into the key store with the specified alias and protection parameters.
-     *
-     * @param alias the alias for the key
-     * @param key the key to be inserted
-     * @param base64 whether the key is encoded in base64
-     * @param password the password to protect the key
-     * @param salt the salt to derive the password
-     */
-    private void insertKey(String alias, byte[] key, boolean base64, byte[] password, byte[] salt) {
-        char[] fullPassword = CryptoUtils.concatAndConvertPasswordAndSalt(base64, password, salt);
-
-        KeyStore.SecretKeyEntry secretKeyEntry = new KeyStore.SecretKeyEntry(new SecretKeySpec(key, "AES"));
-        KeyStore.ProtectionParameter protParam = new KeyStore.PasswordProtection(fullPassword);
+        char[] totpKeyPassword = DataUtils.concatArray(this.sessionManager.getUserAuthKey().getTotpEncryptionKeyChar(),
+                this.sessionManager.getKeyStoreConfig().getSaltWithTotpEncryptionKeyChar());
 
         try {
-            this.keyStore.setEntry(alias, secretKeyEntry, protParam);
-        } catch (KeyStoreException e) {
-            System.err.println("An error occurred while trying to obtain the key.");
+            this.keyStoreEntryManager.insertKey(keyStore, TOTP_ENCRYPTION_KEY_ALIAS,
+                    this.sessionManager.getUserAuthKey().getTotpEncryptionKey(), totpEncryptionKeyPassword);
+            this.keyStoreEntryManager.insertKey(keyStore, TOTP_KEY_ALIAS,
+                    this.sessionManager.getUserAuthKey().getTotpKey(), totpKeyPassword);
         } finally {
-            CryptoUtils.cleanMemory(fullPassword);
-            secretKeyEntry = null;
-            protParam = null;
+            CryptoUtils.cleanMemory(totpEncryptionKeyPassword);
+            CryptoUtils.cleanMemory(totpKeyPassword);
         }
+
     }
 
     /**
-     * Loads the key store from the file system using the specified username.
-     *
-     * @param username the username to identify the key store file
+     * Saves the key store to the file system.
      */
     @Override
-    public void loadKeyStore(String username) {
-        Path keyStoreFilePath = this.fileManager.createPath(true, PathsConstant.KEY_STORE.getParameter(),
-                username.concat(".bcfks"));
+    public void saveKeyStore() {
+        String fileName = this.sessionManager.getUserAccount().getUsername();
 
-        char[] keyStorePassword = EncodingUtils
-                .byteToCharConverter(this.keyStoreConfiguration.getKeyStoreEncryptionKey());
-
-        try (InputStream in = new FileInputStream(keyStoreFilePath.toFile())) {
-            this.keyStore = KeyStore.getInstance("BCFKS", "BC");
-            this.keyStore.load(in, keyStorePassword);
-        } catch (IOException | KeyStoreException | NoSuchProviderException | NoSuchAlgorithmException
-                | CertificateException e) {
-            System.err.println("An error occurred while trying to open the file.");
-        } finally {
-            CryptoUtils.cleanMemory(keyStorePassword);
-        }
-    }
-
-    /**
-     * Saves the key store to the file system using the specified username.
-     *
-     * @param username the username to identify the key store file
-     */
-    @Override
-    public void saveKeyStore(String username) {
-        Path keyStoreFilePath = this.fileManager.createPath(true, PathsConstant.KEY_STORE.getParameter(),
-                username.concat(".bcfks"));
-
-        this.fileManager.createFolder(keyStoreFilePath);
-
-        char[] keyStorePassword = EncodingUtils
-                .byteToCharConverter(this.keyStoreConfiguration.getKeyStoreEncryptionKey());
-
-        try (OutputStream out = new FileOutputStream(keyStoreFilePath.toFile())) {
-            keyStore.store(out, keyStorePassword);
-        } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException | IOException e) {
-            System.err.println("An error occurred while trying to save the file.");
-        } finally {
-            CryptoUtils.cleanMemory(keyStorePassword);
-        }
-    }
-
-    /**
-     * Obtains the key from the key store with the specified alias and protection parameters.
-     *
-     * @param alias the alias for the key
-     * @param base64 whether the key is encoded in base64
-     * @param password the password to access the key
-     * @param salt the salt to derive the password
-     * @return the obtained key
-     */
-    @Override
-    public byte[] obtainKey(String alias, boolean base64, byte[] password, byte[] salt) {
-        char[] fullPassword = CryptoUtils.concatAndConvertPasswordAndSalt(base64, password, salt);
-
-        byte[] key = null;
+        Path keyStoreFilePath = this.fileManager.createPath(fileName);
 
         try {
-            key = this.keyStore.getKey(alias, fullPassword).getEncoded();
-        } catch (UnrecoverableKeyException | KeyStoreException | NoSuchAlgorithmException e) {
-            System.err.println("An error occurred while trying to obtain the key.");
-        } finally {
-            CryptoUtils.cleanMemory(fullPassword);
+            Files.createDirectories(keyStoreFilePath.getParent());
+        } catch (IOException e) {
+            System.err.println(
+                    "An IO error occurred while trying to create the directory.");
         }
 
-        return key;
+        char[] keyStorePassword = this.sessionManager.getKeyStoreConfig().getKeyStoreEncryptionKeyChar();
+
+        this.keyStoreStorage.saveKeyStore(keyStoreFilePath, keyStore, keyStorePassword);
     }
 
     /**
      * Closes the key store and cleans up resources.
      */
     @Override
-    public void close() {
-        try {
-            while (this.keyStore.aliases().hasMoreElements()) {
-                String alias = this.keyStore.aliases().nextElement();
-                keyStore.deleteEntry(alias);
-            }
-            
-            this.keyStore = null;
+    public void closeKeyStore() {
+        this.keyStoreStorage.closeKeyStore(keyStore);
+        this.keyStore = null;
+    }
 
-            this.getKeyStoreConfiguration().destroy();
-            this.setKeyStoreConfiguration(null);
-        } catch (KeyStoreException e) {
-            System.err.println("An error occurred while trying to delete the key store.");
+    /**
+     * Encrypts the key store configuration using the user's authentication key and algorithm config.
+     */
+    @Override
+    public void encryptConfig() {
+        byte[] serializedData = EncodingUtils.serializeData(this.sessionManager.getKeyStoreConfig());
+
+        byte[] encryptionKey = this.sessionManager.getUserAuthKey().getPasswordDerivedKey();
+        AlgorithmConfig algorithmConfig = this.sessionManager.getUserAuthInfo().getKeyStoreEncryptionConfig();
+
+        byte[] encryptedData = this.keyStoreService.encryptConfig(serializedData, encryptionKey, algorithmConfig);
+
+        this.sessionManager.getUserAuthInfo().setKeyStoreConfigEncryptedData(encryptedData);
+    }
+
+    /**
+     * Decrypts the key store configuration using the user's authentication key and algorithm config.
+     */
+    @Override
+    public void decryptConfig() {
+        byte[] encryptedData = this.sessionManager.getUserAuthInfo().getKeyStoreConfigEncryptedData();
+
+        byte[] encryptionKey = this.sessionManager.getUserAuthKey().getPasswordDerivedKey();
+        AlgorithmConfig algorithmConfig = this.sessionManager.getUserAuthInfo().getKeyStoreEncryptionConfig();
+
+        KeyStoreConfig keyStoreConfig = this.keyStoreService.decryptConfig(encryptedData, encryptionKey,
+                algorithmConfig);
+
+        this.sessionManager.setKeyStoreConfig(keyStoreConfig);
+    }
+
+    /**
+     * Loads the key store from the file system using the specified username.
+     */
+    @Override
+    public void loadKeyStore() {
+        String username = this.sessionManager.getUserAccount().getUsername();
+
+        byte[] keyStoreEncrypted = this.fileManager.loadData(username);
+        char[] keyStorePassword = this.sessionManager.getKeyStoreConfig().getKeyStoreEncryptionKeyChar();
+
+        this.keyStore = this.keyStoreStorage.loadKeyStore(keyStoreEncrypted, keyStorePassword);
+    }
+
+    /**
+     * Retrieves the keys stored in the key store and updates the user authentication key.
+     */
+    @Override
+    public void getKeyStoreKeys() {
+        char[] totpEncryptionKeyPassword = DataUtils.concatArray(
+                this.sessionManager.getUserAuthKey().getPasswordDerivedKeyChar(),
+                this.sessionManager.getKeyStoreConfig().getSaltWithPasswordDerivedChar());
+
+        try {
+            byte[] key = this.keyStoreEntryManager.retrieveKey(keyStore, TOTP_ENCRYPTION_KEY_ALIAS,
+                    totpEncryptionKeyPassword);
+            this.sessionManager.getUserAuthKey().setTotpEncryptionKey(key);
+        } finally {
+            CryptoUtils.cleanMemory(totpEncryptionKeyPassword);
+        }
+
+        char[] totpKeyPassword = DataUtils.concatArray(this.sessionManager.getUserAuthKey().getTotpEncryptionKeyChar(),
+                this.sessionManager.getKeyStoreConfig().getSaltWithTotpEncryptionKeyChar());
+
+        try {
+            byte[] key = this.keyStoreEntryManager.retrieveKey(keyStore, TOTP_KEY_ALIAS, totpKeyPassword);
+            this.sessionManager.getUserAuthKey().setTotpKey(key);
+        } finally {
+            CryptoUtils.cleanMemory(totpKeyPassword);
         }
     }
 
-    /* GETTER and SETTER */
+    /**
+     * Gets the current key store instance.
+     *
+     * @return the current key store
+     */
     public KeyStore getKeyStore() {
         return keyStore;
-    }
-    public KeyStoreConfig getKeyStoreConfiguration() {
-        return keyStoreConfiguration;
-    }
-
-    public void setFileManager(FileManager fileManager) {
-        this.fileManager = fileManager;
-    }
-
-    public void setKeyStoreConfiguration(KeyStoreConfig keyStoreConfiguration) {
-        this.keyStoreConfiguration = keyStoreConfiguration;
     }
 }
