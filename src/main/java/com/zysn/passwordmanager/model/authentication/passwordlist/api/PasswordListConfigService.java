cb4996f2d15300ad787e3283421004b2f93cb68f
ZY <yuhang.zhu@studio.unibo.it>
2025-02-21 13:57:33 +0100

    Add class to decrypt services password list

---
+++ src/main/java/com/zysn/passwordmanager/model/authentication/passwordlist/api/PasswordListConfigService.java
@@ -0,0 +1,37 @@
+package com.zysn.passwordmanager.model.authentication.passwordlist.api;
+
+import com.zysn.passwordmanager.model.authentication.passwordlist.impl.ServiceCryptoConfig;
+import com.zysn.passwordmanager.model.security.algorithm.config.impl.AlgorithmConfig;
+
+/**
+ * Interface for managing password list configurations.
+ */
+public interface PasswordListConfigService {
+
+    /**
+     * Creates a new ServiceCryptoConfig for the password list.
+     *
+     * @return the generated ServiceCryptoConfig
+     */
+    public ServiceCryptoConfig createPasswordListConfig();
+
+    /**
+     * Encrypts the given configuration data.
+     *
+     * @param data the data to encrypt
+     * @param key the encryption key
+     * @param algorithmConfig the algorithm configuration
+     * @return the encrypted data
+     */
+    public byte[] encryptConfig(byte[] data, byte[] key, AlgorithmConfig algorithmConfig);
+
+    /**
+     * Decrypts the given configuration data.
+     *
+     * @param data the data to decrypt
+     * @param key the decryption key
+     * @param algorithmConfig the algorithm configuration
+     * @return the decrypted data
+     */
+    public byte[] decryptConfig(byte[] data, byte[] key, AlgorithmConfig algorithmConfig);
+}
