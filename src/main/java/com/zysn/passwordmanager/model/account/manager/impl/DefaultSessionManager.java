2dc6c99c8bacc3e985e337dd8f9c9d4a14e52014
ZY <yuhang.zhu@studio.unibo.it>
2025-02-23 02:50:52 +0100

    Refactor session manager and account manager

---
+++ src/main/java/com/zysn/passwordmanager/model/account/manager/impl/DefaultSessionManager.java
@@ -0,0 +1,72 @@
+package com.zysn.passwordmanager.model.account.manager.impl;
+
+import com.zysn.passwordmanager.model.account.entity.impl.UserAccount;
+import com.zysn.passwordmanager.model.account.entity.impl.UserAuthInfo;
+import com.zysn.passwordmanager.model.account.entity.impl.UserAuthKey;
+import com.zysn.passwordmanager.model.account.manager.api.SessionManager;
+import com.zysn.passwordmanager.model.authentication.keystore.impl.KeyStoreConfig;
+import com.zysn.passwordmanager.model.authentication.passwordlist.impl.ServiceCryptoConfig;
+
+/**
+ * DefaultSessionManager class that implements the SessionManager interface
+ * to manage user sessions with a default implementation.
+ */
+public class DefaultSessionManager implements SessionManager {
+    private UserAccount userAccount;
+    private UserAuthInfo userAuthInfo;
+    private UserAuthKey userAuthKey;
+    private KeyStoreConfig keyStoreConfig;
+    private ServiceCryptoConfig serviceConfig;
+
+    /**
+     * Constructs a DefaultSessionManager with default values.
+     */
+    public DefaultSessionManager() {
+        this.userAccount = new UserAccount();
+        this.userAuthInfo = new UserAuthInfo();
+        this.userAuthKey = new UserAuthKey();
+        this.keyStoreConfig = new KeyStoreConfig();
+        this.serviceConfig = new ServiceCryptoConfig();
+    }
+
+    public UserAuthInfo getUserAuthInfo() {
+        return userAuthInfo;
+    }
+
+    public void setUserAuthInfo(final UserAuthInfo userAuthInfo) {
+        this.userAuthInfo = userAuthInfo;
+    }
+
+    public UserAuthKey getUserAuthKey() {
+        return userAuthKey;
+    }
+
+    public void setUserAuthKey(final UserAuthKey userAuthKey) {
+        this.userAuthKey = userAuthKey;
+    }
+
+    public KeyStoreConfig getKeyStoreConfig() {
+        return keyStoreConfig;
+    }
+
+    public void setKeyStoreConfig(final KeyStoreConfig keyStoreConfig) {
+        this.keyStoreConfig = keyStoreConfig;
+    }
+
+    public ServiceCryptoConfig getServiceConfig() {
+        return serviceConfig;
+    }
+
+    public void setServiceConfig(final ServiceCryptoConfig serviceConfig) {
+        this.serviceConfig = serviceConfig;
+    }
+
+    public UserAccount getUserAccount() {
+        return userAccount;
+    }
+
+    public void setUserAccount(final UserAccount userAccount) {
+        this.userAccount = userAccount;
+    }
+
+}
