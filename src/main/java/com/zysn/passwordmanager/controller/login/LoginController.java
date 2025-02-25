15549226153f6dfc5c18f10bbb92382cdc5bd54b
ZY <yuhang.zhu@studio.unibo.it>
2025-02-25 01:28:30 +0100

    Update login controller

--- src/main/java/com/zysn/passwordmanager/controller/login/LoginController.java
+++ src/main/java/com/zysn/passwordmanager/controller/login/LoginController.java
@@ -3,18 +3,31 @@ package com.zysn.passwordmanager.controller.login;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import com.zysn.passwordmanager.controller.scene.api.SceneControllerBase;
+import com.zysn.passwordmanager.controller.error.api.ErrorHandler;
+import com.zysn.passwordmanager.controller.error.impl.DefaultErrorHandler;
+import com.zysn.passwordmanager.controller.scene.api.ControllerAbstract;
+import com.zysn.passwordmanager.model.account.entity.impl.CollectedUserData;
+import com.zysn.passwordmanager.model.account.manager.api.AccountManager;
+import com.zysn.passwordmanager.model.authentication.login.api.LoginStepHandler;
+import com.zysn.passwordmanager.model.authentication.login.impl.GeneralLoginHandler;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
- * Sample Skeleton for 'Login.fxml' Controller Class
+ * Controller for handling the login functionality.
+ * 
+ * <p>
+ * This class extends {@link ControllerAbstract} with types {@link Stage} and
+ * {@link AccountManager}.
+ * </p>
  */
-public class LoginController extends SceneControllerBase {
+public class LoginController extends ControllerAbstract<Stage, AccountManager> {
+
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
 
@@ -30,21 +43,25 @@ public class LoginController extends SceneControllerBase {
     @FXML // fx:id="usernameField"
     private TextField usernameField; // Value injected by FXMLLoader
 
-    @FXML
-    void handleLogin(ActionEvent event) {
+    private CollectedUserData collectedUserData;
 
+    private LoginStepHandler loginStepHandler;
+
+    private ErrorHandler errorHandler;
+
+    /**
+     * Initializes data for the login process.
+     */
+    @Override
+    public void initializeData() {
+        this.collectedUserData = new CollectedUserData();
+        this.loginStepHandler = new GeneralLoginHandler(collectedUserData);
+        this.errorHandler = new DefaultErrorHandler();
     }
 
-    @FXML
-    void handleRegister(ActionEvent event) {
-        this.getViewNavigator().navigateTo("/layouts/registration/Register.fxml", "Registration");
-    }
-
-    @FXML
-    void handleRestore(ActionEvent event) {
-
-    }
-
+    /**
+     * Initializes the controller class.
+     */
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert mainVBox != null : "fx:id=\"mainVBox\" was not injected: check your FXML file 'Login.fxml'.";
@@ -52,4 +69,67 @@ public class LoginController extends SceneControllerBase {
         assert usernameField != null : "fx:id=\"usernameField\" was not injected: check your FXML file 'Login.fxml'.";
     }
 
+    /**
+     * Handles the login action.
+     *
+     * @param event the action event triggered by the login button
+     */
+    @FXML
+    void handleLogin(final ActionEvent event) {
+        this.collectUserData();
+
+        try {
+            this.loginStepHandler.handleStep();
+
+            super.getData().login(collectedUserData);
+
+            this.chooseNextView();
+
+        } catch (final IllegalArgumentException e) {
+            if (this.getData().getSessionManager().getUserAccount().getMasterKey() == null) {
+                errorHandler.showError("Errors", "Username or password are wrong.");
+            } else {
+                errorHandler.showError("Errors", e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Handles the registration action.
+     *
+     * @param event the action event triggered by the registration button
+     */
+    @FXML
+    void handleRegister(final ActionEvent event) {
+        this.getNavigator().navigateTo("/layouts/registration/MainRegistrationPage.fxml", "Registration");
+    }
+
+    /**
+     * Handles the restore action.
+     *
+     * @param event the action event triggered by the restore button
+     */
+    @FXML
+    void handleRestore(final ActionEvent event) {
+        // Add restore functionality here
+    }
+
+    /**
+     * Chooses the next view based on the user authentication information.
+     */
+    private void chooseNextView() {
+        if (this.getData().getSessionManager().getUserAuthInfo().isEnabled2FA()) {
+            this.getNavigator().navigateTo("/layouts/login/Login2FA.fxml", "Main");
+        } else {
+            this.getNavigator().navigateTo("/layouts/Main.fxml", "Main");
+        }
+    }
+
+    /**
+     * Collects user data from the login fields.
+     */
+    private void collectUserData() {
+        this.collectedUserData.setUsername(this.usernameField.getText());
+        this.collectedUserData.setPassword(this.passwordField.getText().getBytes());
+    }
 }
