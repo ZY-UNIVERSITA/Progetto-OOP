1bbad54cc43f760e550c687159969054f1c9d83d
ZY <yuhang.zhu@studio.unibo.it>
2025-02-25 20:30:22 +0100

    Add last step for authentication

--- src/main/java/com/zysn/passwordmanager/model/authentication/login/impl/DefaultLoginService.java
+++ src/main/java/com/zysn/passwordmanager/model/authentication/login/impl/DefaultLoginService.java
@@ -15,6 +15,7 @@ import com.zysn.passwordmanager.model.authentication.common.impl.GetKeyFromKeySt
 import com.zysn.passwordmanager.model.authentication.common.impl.InsertUserData;
 import com.zysn.passwordmanager.model.authentication.common.impl.LoadKeyStore;
 import com.zysn.passwordmanager.model.authentication.common.impl.LoadUserData;
+import com.zysn.passwordmanager.model.authentication.common.impl.LoadUserPassword;
 import com.zysn.passwordmanager.model.authentication.keystore.api.KeyStoreManager;
 import com.zysn.passwordmanager.model.authentication.keystore.impl.DefaultKeyStoreManager;
 import com.zysn.passwordmanager.model.authentication.login.api.LoginService;
@@ -22,16 +23,26 @@ import com.zysn.passwordmanager.model.authentication.passwordlist.api.ServiceCry
 import com.zysn.passwordmanager.model.authentication.passwordlist.impl.DefaultServiceCryptoConfigManager;
 import com.zysn.passwordmanager.model.security.manager.CryptoManager;
 
+/**
+ * The DefaultLoginService class implements the LoginService interface.
+ * It provides the default implementation for the user login process, managing various authentication steps.
+ */
 public class DefaultLoginService implements LoginService {
-private List<AuthenticationStep> authenticationStep;
-    private KeyStoreManager keyStoreManager;
-    private ServiceCryptoConfigManager passwordListConfigManager;
+    private final List<AuthenticationStep> authenticationStep;
+    private final KeyStoreManager keyStoreManager;
+    private final ServiceCryptoConfigManager passwordListConfigManager;
 
-    private CryptoManager cryptoManager;
-    private SessionManager sessionManager;
+    private final CryptoManager cryptoManager;
+    private final SessionManager sessionManager;
     private CollectedUserData collectedUserData;
-    
-    public DefaultLoginService(SessionManager sessionManager) {
+
+    /**
+     * Constructs a DefaultLoginService object with the specified session manager.
+     * Initializes the necessary managers and prepares the list of authentication steps.
+     * 
+     * @param sessionManager the SessionManager to manage user sessions
+     */
+    public DefaultLoginService(final SessionManager sessionManager) {
         this.authenticationStep = new ArrayList<>();
         this.keyStoreManager = new DefaultKeyStoreManager(sessionManager);
         this.passwordListConfigManager = new DefaultServiceCryptoConfigManager(sessionManager);
@@ -40,26 +51,69 @@ private List<AuthenticationStep> authenticationStep;
         this.sessionManager = sessionManager;
     }
 
+    /**
+     * Logs in the user with the provided collected user data.
+     * Executes various authentication steps to complete the login process.
+     * 
+     * @param collectedUserData the CollectedUserData object containing user data for login
+     */
     @Override
-    public void login(CollectedUserData collectedUserData) {
+    public void login(final CollectedUserData collectedUserData) {
         this.collectedUserData = collectedUserData;
 
-        this.loginStep();
+        this.loadUserPersonalData();
+        this.deriveKeyFromPassword();
+        this.loadKeyStore();
+        this.decryptServiceConfig();
+        this.deriveMasterKey();
+        this.loadUserPassword();
 
         this.authenticationStep.forEach(AuthenticationStep::executeStep);
-    }    
+    }
 
-    private void loginStep() {
+    /**
+     * Adds steps to load user personal data to the authentication steps.
+     */
+    private void loadUserPersonalData() {
         authenticationStep.add(new InsertUserData(sessionManager, collectedUserData));
         authenticationStep.add(new LoadUserData(sessionManager));
+    }
+
+    /**
+     * Adds steps to derive a key from the user's password to the authentication steps.
+     */
+    private void deriveKeyFromPassword() {
         authenticationStep.add(new DeriveKeyFromPassword(sessionManager, cryptoManager));
+    }
+
+    /**
+     * Adds steps to load the key store to the authentication steps.
+     */
+    private void loadKeyStore() {
         authenticationStep.add(new DecryptKeyStoreConfig(sessionManager, keyStoreManager));
         authenticationStep.add(new LoadKeyStore(sessionManager, keyStoreManager));
         authenticationStep.add(new GetKeyFromKeyStore(sessionManager, keyStoreManager));
+    }
+
+    /**
+     * Adds steps to decrypt the service configuration to the authentication steps.
+     */
+    private void decryptServiceConfig() {
         authenticationStep.add(new DeriveServiceConfigKey(sessionManager, cryptoManager));
         authenticationStep.add(new DecryptServiceConfig(sessionManager, passwordListConfigManager));
+    }
+
+    /**
+     * Adds a step to derive the master key to the authentication steps.
+     */
+    private void deriveMasterKey() {
         authenticationStep.add(new DeriveMasterKey(sessionManager, cryptoManager));
     }
-    
-    
+
+    /**
+     * Adds a step to load the user's password to the authentication steps.
+     */
+    private void loadUserPassword() {
+        authenticationStep.add(new LoadUserPassword(sessionManager, false));
+    }
 }
